c5503659f1638c4a3d7b0374fd60e7418ece59cd
zishuixing <840679833@qq.com>
2026-04-10 18:50:16 +0800

    feat: 29 修复写入flash问题(二) 分批次写入多字节

--- Interface/Int_boot_loader.c
+++ Interface/Int_boot_loader.c
@@ -4,8 +4,15 @@
 uint8_t rx_buf[BOOTLOADER_RX_BUF_SIZE];
 uint16_t rx_len = 0;
 
+uint16_t rx_len_Size = 0;
+
 // 记录当前写入程序的偏移量
 uint32_t flash_write_offset = 0;
+
+// 最后一个字节是否单独处理
+uint8_t is_last_byte = 0;
+// flag 标志位
+uint8_t is_flag_byte = 0;
 
 void Int_boot_loader_Init(void)
 {
@@ -17,6 +24,65 @@ void Int_boot_loader_Init(void)
     // 初始化bootloader
     // usart 接收函数
     HAL_UARTEx_ReceiveToIdle_IT(&huart1, rx_buf, BOOTLOADER_RX_BUF_SIZE);
+}
+
+// 上一次没有剩余 则需要单独处理函数
+void write_without_last_byte(void)
+{
+    for (uint32_t i = 0; i < rx_len_Size; i += 2)
+    {
+        uint32_t flash_addr = APPLICATION_FLASH_START_ADDR + flash_write_offset + i;
+        uint16_t data16;
+
+        if (i + 1 < rx_len_Size)
+        {
+
+            // 写入16位数据
+            data16 = (rx_buf[i]) | (rx_buf[i + 1] << 8);
+            // 写入flash
+            HAL_StatusTypeDef status = HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, flash_addr, data16);
+            if (status != HAL_OK)
+            {
+                // 抓到了！在这里打断点，查看是配置错误还是Flash被锁了
+                uint32_t error_code = HAL_FLASH_GetError();
+                // 打印错误码
+                printf("Flash Error Code: 0x%08X\n", error_code);
+                break;
+            }
+        }
+    }
+}
+
+// 上一次有剩余 则需要单独处理函数
+void write_with_last_byte(void)
+{
+    // 1  3 或者 1  2
+    for (uint32_t i = 0; i < rx_len_Size; i += 2)
+    {
+        uint32_t flash_addr = APPLICATION_FLASH_START_ADDR + flash_write_offset + i;
+        uint16_t data16;
+
+        if (i == 0)
+        {
+            // 第一个字节 直接写入
+            data16 = is_last_byte | (rx_buf[0] << 8);
+        }
+        else
+        {
+            // 其他字节 直接写入
+            data16 = (rx_buf[i - 1]) | (rx_buf[i] << 8);
+        }
+        // 写入flash
+        HAL_StatusTypeDef status = HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, flash_addr, data16);
+        if (status != HAL_OK)
+        {
+            // 抓到了！在这里打断点，查看是配置错误还是Flash被锁了
+            uint32_t error_code = HAL_FLASH_GetError();
+            // 打印错误码
+            printf("Flash Error Code: 0x%08X\n", error_code);
+            break;
+        }
+    }
 }
 
 /**
@@ -34,6 +100,7 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
     // 接收完成回调函数
     if (huart->Instance == USART1)
     {
+        rx_len_Size = Size;
         rx_len += Size;
 
         // 接收到数据
@@ -80,36 +147,54 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
             // flash擦除比较耗费性能
             HAL_FLASHEx_Erase(&erase_init, &page_error);
         }
-        // 2.3 使用16位写入 => 比较贴合实际情况
-        for (uint32_t i = 0; i < Size; i += 2)
-        {
-            uint32_t flash_addr = APPLICATION_FLASH_START_ADDR + flash_write_offset + i;
-            uint16_t data16;
-            if (i + 1 < Size)
-            {
-                // 写入16位数据
-                data16 = (rx_buf[i]) | (rx_buf[i + 1] << 8);
-            }
 
+        // 2.3 写入数据 处理最后一个字节问题
+        if ((Size + is_flag_byte) % 2 == 0)
+        {
+            if (is_flag_byte == 1)
+            {
+                // 上一次有剩余 则需要单独处理
+                write_with_last_byte();
+                // 记录偏移量
+                flash_write_offset += Size + 1;
+                is_flag_byte = 0;
+            }
             else
             {
-                // 最后一个字节出现了单独的情况
-                // 写入8位数据
-                data16 = rx_buf[i] | (0xff << 8);
-            }
-            // 写入flash
-            HAL_StatusTypeDef status = HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, flash_addr, data16);
-            if (status != HAL_OK)
-            {
-                // 抓到了！在这里打断点，查看是配置错误还是Flash被锁了
-                uint32_t error_code = HAL_FLASH_GetError();
-                // 打印错误码
-                printf("Flash Error Code: 0x%08X\n", error_code);
-                break;
+                // 上一次没有剩余 则直接写入
+                write_without_last_byte();
+                // 记录偏移量
+                flash_write_offset += Size;
+
+                // 代表写入无单独处理的字节
+                is_flag_byte = 0;
             }
         }
-        // 2.4  记录偏移量
-        flash_write_offset += Size;
+
+        else
+        {
+
+            if (is_flag_byte == 1)
+            {
+                // 上一次有剩余 则需要单独处理 且这次也剩余一个字节
+                write_with_last_byte();
+                // 记录偏移量
+                flash_write_offset += Size;
+            }
+            else
+            {
+                // 上一次没有剩余 则直接写入 且这次也剩余一个字节
+                write_without_last_byte();
+                // 记录偏移量
+                flash_write_offset += Size - 1;
+            }
+
+            // 代表下次有单独处理的字节
+            is_last_byte = rx_buf[Size - 1];
+            is_flag_byte = 1;
+        }
+
+        // 2.3 使用16位写入 => 比较贴合实际情况
 
         // 加锁
         HAL_FLASH_Lock();
